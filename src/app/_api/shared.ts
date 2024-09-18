464274b85dcf6e8e6b6f49f55b2d0411a7abcac6
AntoniWachowicz <antwachowicz@gmail.com>
2024-09-18 16:23:41 +0200

    Update shared.ts

--- src/app/_api/shared.ts
+++ src/app/_api/shared.ts
@@ -1,3 +1,5 @@
-export const GRAPHQL_API_URL = process.env.NEXT_BUILD
-  ? `http://localhost:${process.env.PORT || 3000}`
-  : process.env.NEXT_PUBLIC_SERVER_URL
+export const GRAPHQL_API_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL ||
+  (process.env.NEXT_BUILD
+    ? `http://localhost:${process.env.PORT || 3000}`
+    : 'https://lgdbudujrazem.vercel.app')
